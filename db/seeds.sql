8cbf2b96f58dafe2a78a2c08b7676a0734dc34e7
Truc Nguyen <trucn0215@gmail.com>
2021-01-10 19:52:49 -0800

    create connection.js file and setup code to connect to Mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,6 +4,6 @@ INSERT INTO burgers (burger_name, devoured)
     VALUES
         ("The Madlove Burger", 0),
         ("Smoke & Pepper", 0),
-        ("Banzai", 1)
+        ("Banzai", 1);
 
 SELECT * FROM burgers;
