ea7a27bc842e1f39bdb01487fd386a39a08628bd
Truc Nguyen <trucn0215@gmail.com>
2021-01-10 20:10:10 -0800

    create same methods that are in orm.js to connecting the 2 files

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,17 @@
 const orm = require("../config/orm.js");
 
+const burger = {
+    selectAll: function(){
+
+    },
+
+    insertOne: function(){
+
+    },
+
+    updateOne: function(){
+
+    }
+}
+
+module.exports = burger;
