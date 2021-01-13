b0e11c0816a49830ab5400a75b8a988ab7c910fb
Truc Nguyen <trucn0215@gmail.com>
2021-01-12 23:13:23 -0800

    write html code for app view in main.handlebars and index.handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT AUTO_INCREMENT NOT NULL,
     burger_name CHAR (30) NOT NULL,
-    devoured TINYINT(1),
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
