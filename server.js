7d5d0b385b73ee98c904acacced1959bc948450d
Truc Nguyen <trucn0215@gmail.com>
2021-01-12 20:55:23 -0800

    set up inital code for server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-const mysql = require ("mysql");
 const express = require ("express");
-const handlebars = require ("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Set Hanlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Import routes and give server access to them
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Listen on Port: ", PORT);
+})
